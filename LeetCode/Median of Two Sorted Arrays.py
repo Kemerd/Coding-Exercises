2c76fbb19547f5af2eb65de8d2d1e8b40f3d71af
D Everett Hinton <Dragonfabledonny@hotmail.com>
2019-04-04 02:27:07 -0700

    Update Median of Two Sorted Arrays.py

--- LeetCode/Median of Two Sorted Arrays.py
+++ LeetCode/Median of Two Sorted Arrays.py
@@ -1,6 +1,8 @@
 '''
 = D. Everett Hinton =
 
+Difficulty: Hard
+
 https://leetcode.com/submissions/detail/219967799/
 
 Runtime: 60 ms, faster than 98.72% of Python3 online submissions for Median of Two Sorted Arrays.
